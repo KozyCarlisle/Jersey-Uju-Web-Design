c932379e3e6682e8410f75c93a9f78f3dbe453fa
Jersey Uju <kozycarlisle@gmail.com>
2026-01-04 20:06:31 +0000

    Add files via upload

---
+++ web-design/wp_launchpad/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("domainLink").addEventListener("click", function () {
+  window.location.href = "webhosting.html";
+});
+
+document.getElementById("domainLink1").addEventListener("click", function () {
+  window.location.href = "about.html";
+});
+
+document.getElementById("domainLink1").addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
+
+document.getElementById("domainLink1").addEventListener("click", function () {
+  window.location.href = "support.html";
+});
